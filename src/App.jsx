95d815c49d502fecad68df3609af3b3bbd1ab72b
lhb1810 <lehuubao2909@gmail.com>
2023-02-23 20:00:26 +0700

    add theme light

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useContext } from 'react'
 import './App.css'
 import Nav from './components/Nav'
 import CardTask from './components/CardTask'
 import Home from './pages/Home'
+import { ThemeContext } from './components/ThemeContext'
 
 
 function App() {
-
+  const { theme } = useContext(ThemeContext)
+  
   return (
-    <div className="App dark">
+    <div className={`App ${theme}`}>
       <Home />
     </div>
   )
