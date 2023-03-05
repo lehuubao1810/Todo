73b0f0eaaa0f5a24766288d5f1049c03e18a8d31
lhb1810 <lehuubao2909@gmail.com>
2023-03-05 13:30:08 +0700

    remove DataContext

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import App from './App'
 import './index.css'
 import ThemeProvider from './contexts/ThemeContext'
 import { AuthProvider } from './contexts/AuthContext'
-import { DataProvider } from './contexts/DataContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <DataProvider>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </DataProvider>
   </AuthProvider>
 )
